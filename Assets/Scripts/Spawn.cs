38727f24dc4381c6e7c4ae1e26c34e2b5a0fd8a7
ha <some@email.com>
2019-08-28 00:31:36 +0300

    update script Spawn

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -4,39 +4,80 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
-
     public GameObject[] myBlocks;
     public int variantSpawn;
     public int numbersChooseBlock;
     public int[] numberBlock;
+    public bool SerchShare = true;
+    public int index;
 
     void Start()
     {
         Next();
     }
 
-   
+
     void Update()
     {
- 
+
     }
 
-    public void NextOfChooseOfShare()
+    public void RememberShareZ()
+    { 
+        for(int i=0;i<=numbersChooseBlock;i++)
+        if(numberBlock[i]==0)
+                SerchShare =false;
+        if (SerchShare)
+        {
+            numberBlock[numbersChooseBlock + 1] = 0;
+            numbersChooseBlock++;
+        }
+    }
+    public void RememberShareO()
     {
-
+        for (int i = 0; i <= numbersChooseBlock; i++)
+            if (numberBlock[i] == 1)
+                SerchShare = false;
+            if (SerchShare)
+            {
+              numberBlock[numbersChooseBlock + 1] = 1;
+              numbersChooseBlock++;
+            }
+}
+    public void RememberShareI()
+    {
+        for (int i = 0; i <= numbersChooseBlock; i++)
+            if (numberBlock[i] == 2) SerchShare = false;
+        if (SerchShare)
+        { 
+            numberBlock[numbersChooseBlock + 1] = 2;
+            numbersChooseBlock++;
+        }
+    }
+    public void RememberShareT()
+    {
+        for (int i = 0; i <= numbersChooseBlock; i++)
+            if (numberBlock[i] == 0) SerchShare = false;
+        if (SerchShare)
+        { 
+            numberBlock[numbersChooseBlock + 1] = 3;
+            numbersChooseBlock++;
+        }
     }
     
     public void Next()
     {
         if (variantSpawn == 1)
         {
-
+            index = Random.Range(0, numbersChooseBlock);
+            Instantiate(myBlocks[numberBlock[index]], transform.position, Quaternion.identity);
         }
         else
         {
-            int index = Random.Range(0, myBlocks.Length);
+            index = Random.Range(0, myBlocks.Length);
             Instantiate(myBlocks[index], transform.position, Quaternion.identity);
         }
+   
     }
 
 
